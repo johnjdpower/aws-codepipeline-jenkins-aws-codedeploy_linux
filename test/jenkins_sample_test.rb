0574d88245d57bb5f6da9764f5abd32e2084d23e
EC2 Default User <ec2-user@ip-172-31-4-162.ec2.internal>
2016-06-09 13:40:50 +0000

    Changed test to check for completed text

--- test/jenkins_sample_test.rb
+++ test/jenkins_sample_test.rb
@@ -15,6 +15,6 @@ class JenkinsSampleTest < MiniTest::Unit::TestCase
   end
 
   def test_congratulations
-    assert(@webpage =~ /Congratulations123/)
+    assert(@webpage =~ /Completed/)
   end
 end
